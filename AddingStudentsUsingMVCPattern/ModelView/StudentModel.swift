3c1a1f4c3b84b93511a90b33dcdb648be0ce8035
vikram <vikramkunwar1404@gmail.com>
2022-10-06 16:53:29 +0530

    Student List

---
+++ AddingStudentsUsingMVCPattern/ModelView/StudentModel.swift
@@ -0,0 +1,19 @@
+//
+//  StudentModel.swift
+//  AddingStudentsUsingMVCPattern
+//
+//  Created by Vikram Kunwar on 06/10/22.
+//
+
+import Foundation
+
+class Student{
+    var name: String
+    var address: String
+    
+    init(aname: String, Aaddress: String) {
+        
+        name = aname
+        address = Aaddress
+    }
+}
--- AddingStudentsUsingMVCPattern/ViewController.swift
+++ AddingStudentsUsingMVCPattern/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var StudentTableV: UITableView!
+    
+    var students: [Student] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
+    @IBAction func AddStudent(_ sender: UIBarButtonItem) {
+        
+        let newStudnet = Student(aname: "Student \(students.count + 1)", Aaddress: "Address\(students.count + 1)")
+        students.append(newStudnet)
+        StudentTableV.reloadData()
+        
+    }
+}
+extension ViewController: UITableViewDelegate,UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return students.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = students[indexPath.row].name
+        cell?.detailTextLabel?.text = students[indexPath.row].address
+        return cell!
+    }
 }
 
